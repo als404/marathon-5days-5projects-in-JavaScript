257b5234cd9d1428db56fe35fdeee7a44d4bd2b3
Alexander Kolos <7333003@gmail.com>
2022-10-05 17:11:24 +0300

    добавил комментарии

--- 02-drag-drop/app.js
+++ 02-drag-drop/app.js
@@ -3,40 +3,47 @@
 const item = document.querySelector('.item');
 const placeholders = document.querySelectorAll('.placeholder');
 
-item.setAttribute('draggable', 'true');
-item.addEventListener('dragstart', dragstart);
-item.addEventListener('dragend', dragend);
+item.setAttribute('draggable', 'true'); // добавляем возможность перетаскивать объект
+item.addEventListener('dragstart', dragstart); // перетаскиваем объект
+item.addEventListener('dragend', dragend); // отпускаем объект
 
 for (let placeholder of placeholders) {
-    placeholder.addEventListener('dragover', dragover);
-    placeholder.addEventListener('dragenter', dragenter);
-    placeholder.addEventListener('dragleave', dragleave);
-    placeholder.addEventListener('drop', dragdrop);
+    placeholder.addEventListener('dragover', dragover); // над зоной куда можно переместить
+    placeholder.addEventListener('dragenter', dragenter); //начал перемещение
+    placeholder.addEventListener('dragleave', dragleave); // покинул зону
+    placeholder.addEventListener('drop', dragdrop); // закончил перемещение
 }
 
+// начинаем перетаскивать объект
 function dragstart(e) {
     e.target.classList.add('hold');
+    // скрываем объект из области из которой мы его перетягиваем
     setTimeout(() => {
         e.target.classList.add('hide');
-    }, 0)
+    }, 0);
 }
 
+// отпускаем объект
 function dragend(e) {
     e.target.classList.remove('hold', 'hide');
 }
 
+// событие срабатывает куда мы можем поместить данный объект 
 function dragover(e) {
     e.preventDefault();
 }
 
+//событие срабатывает срабатывает когда мы начинаем перетаскивать
 function dragenter(e) {
     e.target.classList.add('hovered');
 }
 
+// срабатывыает событие как только мы покидаем зону в котрую можем переместить объект
 function dragleave(e) {
     e.target.classList.remove('hovered');
 }
 
+// срабатывает событие когда мы заканчиваем перемещение объекта
 function dragdrop(e) {
     e.target.classList.remove('hovered');
     e.target.append(item);

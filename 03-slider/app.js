56b79bdfbd5f9ea9307e34a306de1e0d4b7ff4e4
Alexander Kolos <7333003@gmail.com>
2022-10-06 17:56:19 +0300

    добавил скрипт слайдера

---
+++ 03-slider/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const downBtn = document.querySelector('.controls .down-button'),
+      upBtn  = document.querySelector('.controls .up-button'),
+      sidebar = document.querySelector('.sidebar'),
+      mainSlide = document.querySelector('.main-slide'),
+      container = document.querySelector('.container'),
+      slidesCount = mainSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    const heigth = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * heigth}px)`;
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * heigth}px)`;
+}
